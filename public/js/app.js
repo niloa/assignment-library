9e9e7e39f927853df70d669fd7bd68e6a21c1eda
Suhas <suhas.hm109@gmail.com>
2014-08-29 18:53:12 -0500

    directory structure for Front end

---
+++ public/js/app.js
@@ -0,0 +1,30 @@
+var learningModule = angular.module('learningModule',['ngRoute','ngResource']);
+
+//config routes using angular routeProvider
+
+learningModule.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+
+    $routeProvider.when('/search', {
+        templateUrl: 'partials/searchAssignments.html'
+    })
+        .when('/upload', {
+            templateUrl: 'partials/uploadAssignment.html'
+        })
+        .when('/dqp',{
+            templateUrl: 'partials/DQP.html'
+        })
+        .when('/login',{
+            templateUrl: 'partials/login.html'
+        })
+        .when('/register',{
+            templateUrl: 'partials/signup.html'
+        })
+        .when('/', {
+            templateUrl: 'partials/niloa.html'
+        })
+        .otherwise({
+           redirectTo: '/'
+        });
+
+   //$locationProvider.html5Mode(true);
+}]);
