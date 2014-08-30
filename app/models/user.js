80a84df15ec220c1f581fee2cec9050f3a9edd66
Suhas <suhas.hm109@gmail.com>
2014-08-29 21:34:25 -0500

    Basic Local Authentication_v2

--- app/models/user.js
+++ app/models/user.js
@@ -1,4 +1,24 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
 
-/**
- * Created by nomad on 8/29/2014.
- */
+//Define our User Schema
+var userSchema = mongoose.Schema({
+
+    local: {
+        email   : String,
+        password    : String
+
+    }
+});
+
+//generating hash password
+userSchema.methods.generateHash = function(password){
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+//checking if password is valid
+userSchema.methods.validPassword = function (password) {
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
