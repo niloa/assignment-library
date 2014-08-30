486633b12202d9fba076972e203f1c81d3047dcb
Sharanya Bathey <sharanya.bathey@gmail.com>
2014-08-30 10:44:35 -0500

    Added the lookup route for all tags

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,29 @@
 
 module.exports = function (app, passport) {
 
-//var router = express.Router();  				// get an instance of the express Router
+	// get an instance of the express Router
+	var router = express.Router(); 
 
-// Default document when server is loaded
-    app.get('/', function (req, res) {
-        res.render('index.html');
-    });
+	// Default document when server is loaded
+	router.get("/", function (req, res) {
+        	res.render('index.html');
+    	});
+	
+	router.use(function(req, res, next) {
+		console.log("Starting up the app.");
+		next();	
+	});
+
+	var Tags = require("./app/models/tags");
+	router.route("/tags")
+		.get(function(req, res) {
+			Tags.find(err, tags) {
+				if(err) {
+					res.send({message: "Oops something went wrong please refresh and try!"});
+				res.json(tags);
+			}
+		});
+
+
+	app.use("/api", router);
 }
