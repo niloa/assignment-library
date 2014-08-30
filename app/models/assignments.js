83c1b38550d5a35f6d7d00eee96b7bf950bef65f
Sharanya Bathey <sharanya.bathey@gmail.com>
2014-08-30 09:29:31 -0500

    Added assignments collection javascript

---
+++ app/models/assignments.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var Tags = require("./app/models/tags");
+
+var assignmentsSchema = new Schema({
+	name: {
+		type: String,
+		required: true
+	},
+	description: {
+		type: String,
+		required: true
+	},
+	file_location: {
+		type: String,
+		required: true
+	},
+	created_at: {
+		type: Date,
+		required: true
+	},
+	tags: [{type: Schema.Types.ObjectId, ref: 'Tags' }]
+});
+
+module.exports = mongoose.model('Assignments', assignmentsSchema);
