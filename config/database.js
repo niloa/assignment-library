4abe2d4a7d9ab43b975a39e616257bbe2a894aca
Suhas <suhas@host-1-22.mimpvbg.clients.pavlovmedia.com>
2014-11-19 16:46:51 -0600

    Fixed ngTableParams issue in tagDetailsController

--- config/database.js
+++ config/database.js
@@ -3,4 +3,5 @@ module.exports = {
     //'url': 'mongodb://niloa:nilao@proximus.modulusmongo.net:27017/ne2hYgub'
     //'url': 'mongodb://niloa:nilao@proximus.modulusmongo.net:27017/davA9zex'
 
+
 };
