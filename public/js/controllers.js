028d1a4291327757ba77aa9a487462fa496bec23
Sharanya Bathey <sharanya.bathey@gmail.com>
2015-09-07 22:52:27 -0500

    Fixing no assignments found bug

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -38,6 +38,8 @@ assignmentLibraryControllers.controller("SearchController", function($scope, $ht
     $scope.search = function() {
         $http.get('api/assignments/name/'+ $scope.search_key)
             .success(function(assignments){
+                assignmentsListService.setDataLoaded(true);
+                assignmentsListService.setDisplayTable(assignments !== undefined && assignments.length > 0);
                 assignmentsListService.setAssignments(assignments);
                 $location.path("/assignments/name/" + $scope.search_key);
             })
@@ -48,26 +50,18 @@ assignmentLibraryControllers.controller("SearchController", function($scope, $ht
 });
 
 assignmentLibraryControllers.controller("TagsController", function($scope, $routeParams, $http,  assignmentsListService){
+    $scope.dataLoaded = assignmentsListService.isDataLoaded();
+    $scope.displayTable = assignmentsListService.canDisplayTable();
     $scope.assignments = assignmentsListService.getAssignments();
-    if($scope.assignments!= undefined)
-        $scope.tableDisplay = true;
-
-    // Hide the pagination, not working right now, check later
-    // $scope.tableParams = new ngTableParams({
-    //     count: 0
-    // }, {
-
-    //     counts: [], // hides page sizes
-    //     total: 1
-    // });
     
     if($routeParams.tagId !== undefined) {
         $http.get('api/tags/'+ $routeParams.tagId)
         .success(function(assignments){
+            $scope.dataLoaded = true;
                 if (assignments.length !== 0) {
-                    $scope.tableDisplay = true;
+                    $scope.displayTable = true;
                 } else {
-                    $scope.tableDisplay = false;
+                    $scope.displayTable = false;
                 }
                 $scope.assignments = assignments;
         })
@@ -78,10 +72,6 @@ assignmentLibraryControllers.controller("TagsController", function($scope, $rout
 });
 
 assignmentLibraryControllers.controller("AssignmentDetailsController", function($scope, $modal, $log, $routeParams, $http, $location, $sce, assignmentsLocationService) {
-//$scope.open = function () {
-//    $location.path("/survey");
-//};
-
     $scope.open = function (size) {
         var modalInstance = $modal.open({
             templateUrl: 'myModalContent.html',
@@ -118,115 +108,6 @@ assignmentLibraryControllers.controller('ModalInstanceCtrl', function ($scope, $
         $modalInstance.dismiss('cancel');
     };
 });
-
-//assignmentLibraryControllers.controller("SurveyController", function($scope, $http, $location, assignmentsLocationService) {
-//    $scope.fourYear = false;
-//    $scope.twoYear = false;
-//    $scope.research = false;
-//    $scope.teaching = false;
-//    $scope.vocationalOrTechnical = false;
-//    $scope.none = false;
-//    $scope.insititutionOther = false;
-//    $scope.collegue = false;
-//    $scope.newsletter = false;
-//    $scope.email = false;
-//    $scope.webSearch = false;
-//    $scope.conference = false;
-//    $scope.periodical = false;
-//    $scope.heardFromOther = false;
-//    $scope.fileDownloadLink = assignmentsLocationService.getAssignmentLocation();
-//
-//    var validate = function validateEmail(email) {
-//        if (email === undefined || email === "" || email === null) {
-//            // console.log("came here")
-//            return true;
-//        }
-//        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-//        return re.test(email);
-//    }
-//
-//    $scope.ok = function() {
-//        if (($scope.category !== undefined) && ($scope.fourYear !== false || $scope.twoYear !== false
-//            || $scope.research !== false || $scope.teaching !== false || $scope.vocationalOrTechnical !== false
-//            || $scope.none !== false || $scope.institutionOther !== false) && ($scope.collegue !== false
-//            || $scope.newsletter !== false || $scope.email !== false || $scope.webSearch !== false
-//            || $scope.conference !== false || $scope.periodical !== false || $scope.heardFromOther !== false)
-//            && validate($scope.emailAddress)) {
-//            //
-//            var form = {
-//                category: "",
-//                institution: {},
-//                heardFrom: {},
-//                emailAddress: ""
-//            };
-//
-//            if ($scope.fourYear === true)
-//                form.institution.four_year = true;
-//
-//            if ($scope.twoYear === true)
-//                form.institution.two_year = true;
-//
-//            if ($scope.research === true)
-//                form.institution.research = true;
-//
-//            if ($scope.teaching === true)
-//                form.institution.teaching = true;
-//
-//            if ($scope.vocational_or_technical === true)
-//                form.institution.vocational_or_technical = true;
-//
-//            if ($scope.institutionOther === true)
-//                form.institution.other = true;
-//
-//            if ($scope.none === true)
-//                form.institution.none = true;
-//
-//            form.category = $scope.category;
-//
-//            if ($scope.collegue === true)
-//                form.heardFrom.collegue = true;
-//
-//            if ($scope.newsletter === true)
-//                form.heardFrom.newsletter = true;
-//
-//            if ($scope.email === true)
-//                form.heardFrom.email = true;
-//
-//            if ($scope.webSearch === true)
-//                form.heardFrom.web_search = true;
-//
-//            if ($scope.conference === true)
-//                form.heardFrom.conference = true;
-//
-//            if ($scope.periodical === true)
-//                form.heardFrom.periodical = true;
-//
-//            if ($scope.heardFromOther === true)
-//                form.heardFrom.other = true;
-//
-//            form.emailAddress = $scope.emailAddress;
-//
-//            $http.post('api/survey/', form)
-//            .success(function(status) {
-//                    ($('#download-url')[0]).click();
-//                    $location.path(assignmentsLocationService.getAssignmentPage());
-//            })
-//            .error(function(error) {
-//                console.log(error);
-//            });
-//        } else {
-//            if (!validate($scope.emailAddress)) {
-//                toastr.error("Invalid email address!");
-//            } else {
-//                toastr.error("Some required fields are missing!");
-//            }
-//        }
-//    }
-//
-//    $scope.cancel = function() {
-//        $location.path("/search");
-//    }
-//});
 
 assignmentLibraryControllers.controller("AppCtrl", function ($q, $timeout) {
     var one = $q.defer();
