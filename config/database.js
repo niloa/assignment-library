d9809f382196d9314da0a89c01b4672bc5670044
richa <rsehgal2@illinois.edu>
2016-04-13 01:50:36 -0700

    ff

--- config/database.js
+++ config/database.js
@@ -1,3 +1,5 @@
 module.exports = {
-    'url': 'mongodb://localhost/libAuth'
+    //'url': 'mongodb://localhost/libAuth'
+
+    'url': 'mongodb://niloa:NILOA321@proximus.modulusmongo.net:27017/negyT4yd'
 };
